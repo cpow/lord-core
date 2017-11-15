7d544dfb0a94a097d2432ea5b5840d7787dc3ed0
Chris Power <cpow85@gmail.com>
2017-11-15 14:01:37 -0500

    introduce devise for managers/tenants

---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+  devise_for :property_managers, path: 'property_managers', controllers: {
+    sessions: 'property_managers/sessions',
+    registrations: 'property_managers/registrations'
+  }
+
+  devise_for :users, path: 'users', controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  authenticated :user do
+    root 'high_voltage/pages#show', id: 'user_home', as: :authenticated_user_root
+  end
+
+  authenticated :property_manager do
+    root 'high_voltage/pages#show', id: 'property_manager_home', as: :authenticated_property_manager_root
+  end
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  HighVoltage.configure do |config|
+    config.home_page = 'home'
+  end
+end
---
+++ test/models/property_manager_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PropertyManagerTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
