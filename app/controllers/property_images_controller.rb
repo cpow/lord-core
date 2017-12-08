a95bcd11bcff2ee0b7f1ea3b96e32208319633a3
Chris Power <cpow85@gmail.com>
2017-12-08 12:42:53 -0500

    use carousel on property page. delete property images if you want

--- app/controllers/property_images_controller.rb
+++ app/controllers/property_images_controller.rb
@@ -1,6 +1,7 @@
 class PropertyImagesController < ApplicationController
   before_action :authenticate_property_manager!
   before_action :set_property
+  before_action :set_property_image, only: :destroy
 
   def new
     @property_image = PropertyImage.new
@@ -10,7 +11,16 @@ class PropertyImagesController < ApplicationController
     @property_image = @property.property_images.create(image_params)
   end
 
+  def destroy
+    @property_image_id = @property_image.id
+    @property_image.destroy
+  end
+
   private
+
+  def set_property_image
+    @property_image = PropertyImage.find(params[:id])
+  end
 
   def set_property
     @property = Property.find(params[:property_id])
--- app/models/property_image.rb
+++ app/models/property_image.rb
@@ -15,4 +15,6 @@
 class PropertyImage < ApplicationRecord
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  belongs_to :property
 end
