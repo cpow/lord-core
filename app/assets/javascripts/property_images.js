43c70cf2df8dd3379ab7a4eafea20d6ecc3c6e02
Chris Power <cpow85@gmail.com>
2017-12-23 18:29:31 -0500

    no bueno es6 in sprockets code

--- app/assets/javascripts/property_images.js
+++ app/assets/javascripts/property_images.js
@@ -2,15 +2,15 @@ $(function() {
   $('#new_property_image').fileupload({
     dataType: 'script',
     add: function(e, data) {
-      let source = document.getElementById('template-upload').innerHTML;
-      let template = Handlebars.compile(source);
+      var source = document.getElementById('template-upload').innerHTML;
+      var template = Handlebars.compile(source);
       data.element = $(template({progress: 0}))
       $('#new_property_image').append(data.element);
       data.submit();
     },
     progress: function(e, data) {
       if (data.element) {
-        let progress = parseInt((data.loaded / data.total * 100), 10);
+        var progress = parseInt((data.loaded / data.total * 100), 10);
         data.element.find('.progress-bar').attr('aria-valuenow', progress).css('width', `${progress}%`).text(`${progress}%`);
       }
     },
