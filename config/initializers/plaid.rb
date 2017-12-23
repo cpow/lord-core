2b25fc01318c61fb1417e1b232993b0a4da8c1d8
Chris Power <cpow85@gmail.com>
2017-12-23 17:16:53 -0500

    fix plaid initializer

--- config/initializers/plaid.rb
+++ config/initializers/plaid.rb
@@ -2,6 +2,6 @@
 PLAID = Plaid::Client.new(
   client_id: ENV['PLAID_CLIENT_ID'],
   secret: ENV['PLAID_SECRET'],
-  env: ENV['PLAID_ENV'].to_sym,
+  env: (ENV['PLAID_ENV'] || 'something').to_sym,
   public_key: ENV['PLAID_PUBLIC_KEY']
 )
