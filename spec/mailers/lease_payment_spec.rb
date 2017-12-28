0a9fce6f86462ca09327cb54cbab1d1e0ac512b4
Chris Power <cpow85@gmail.com>
2017-12-28 10:10:13 -0500

    test that all users on a lease get copied to reminder emails

--- spec/mailers/lease_payment_spec.rb
+++ spec/mailers/lease_payment_spec.rb
@@ -1,5 +1,28 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe LeasePaymentMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should email to one user' do
+    user = create(:user, :with_reminder_lease_payment)
+    lease_payment = user.current_lease_payment
+    mailer = described_class.remind_tenants_lease_is_due(lease_payment)
+
+    expect(mailer.to).to eq([user.email])
+    expect(mailer.bcc).to be_empty
+  end
+
+  it 'should email to multiple users if multiple users see lease' do
+    user = create(:user, :with_reminder_lease_payment)
+    residency = user.residencies.last
+    user2 = create(:user)
+    create(:residency,
+           property: residency.property,
+           unit: residency.unit,
+           user: user2)
+    lease_payment = user.current_lease_payment
+
+    mailer = described_class.remind_tenants_lease_is_due(lease_payment)
+
+    expect(mailer.to).to eq([user.email])
+    expect(mailer.bcc).to eq([user2.email])
+  end
 end
