057915111046356dd146c55ddfeaf06433445b9f
Chris Power <cpow85@gmail.com>
2017-12-28 15:17:08 -0500

    use large content for image

--- app/models/property_image.rb
+++ app/models/property_image.rb
@@ -13,7 +13,10 @@
 #
 
 class PropertyImage < ApplicationRecord
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: { large: "700x700>",
+                                      medium: "300x300>",
+                                      thumb: "100x100>" }
+
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   belongs_to :property
