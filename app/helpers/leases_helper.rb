7d4da7b2fea3a071c99377e4ab4b7931f501a4a1
Chris Power <cpow85@gmail.com>
2018-01-31 15:16:52 -0500

    use lease helper for lease views

--- app/helpers/leases_helper.rb
+++ app/helpers/leases_helper.rb
@@ -1,2 +1,15 @@
 module LeasesHelper
+  def badge_for_lease_payment_status(lease_payment)
+    if lease_payment.paid_in_full?
+      content_tag(:span, 'success', class: 'badge badge-primary')
+    elsif lease_payment.payment_late?
+      content_tag(:span, 'late', class: 'badge badge-danger')
+    elsif lease_payment.payment_within_reminder_period?
+      content_tag(:span, 'due soon', class: 'badge badge-warning')
+    elsif lease_payment.active
+      content_tag(:span, 'active', class: 'badge badge-info')
+    else
+      content_tag(:span, 'N/A', class: 'badge badge-secondary')
+    end
+  end
 end
