9355a0d73e83de15a9d63fcbb14ceaef80a2a956
cpow <cpow85@gmail.com>
2019-01-09 16:22:39 +0000

    do not allow property managers to sign up anymore :(

--- app/models/property_manager.rb
+++ app/models/property_manager.rb
@@ -27,8 +27,8 @@
 class PropertyManager < ApplicationRecord
  # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable,
+      :validatable
 
   has_one :notification_subscription, dependent: :destroy
   belongs_to :company, optional: true
